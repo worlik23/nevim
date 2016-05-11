5818a8e711b26f12d97832214aad2ac013c49635
Worlik <Worlik@Worlik-Notes.pslib.cz>
2016-05-11 12:31:01 +0200

    Změna 1

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -14,8 +14,9 @@ public class HelloWorld {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        System.out.println("Ahoj");
     }
     
 }
